bc3c41fd0a97b4c9bd24f635fb3890fb0e3f63d0
rahimkhan007 <41725651+rahimkhan007@users.noreply.github.com>
2018-07-27 12:33:32 +0530

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -9,7 +9,6 @@ class Ideone
 	{
 		int i=0;
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a character:");
 		char ch=sc.next( ).charAt(0);
 		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
 		{
